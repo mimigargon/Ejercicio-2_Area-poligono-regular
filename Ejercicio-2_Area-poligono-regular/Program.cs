40f709be8fe21d29af1f8b974cad2ae2807d29fc
mimigargon <mireiag77@gmail.com>
2023-06-03 12:25:03 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio-2_Area-poligono-regular/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2_Area_poligono_regular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce la apotema: ");
+            double apotema = double.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce el perímetro: ");
+            double perimeter = double.Parse(Console.ReadLine());
+            Console.WriteLine("El área es: " + calculateArea(apotema, perimeter));
+        }
+
+        public static double calculateArea (double a, double p)
+        {
+            double result = 0.0;
+            result = a * p / 2;
+            return result;
+        }
+    }
+}
